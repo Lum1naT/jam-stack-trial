4c4fc55c481240d6e025a4c81f66bcedf8842c57
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2021-01-13 18:57:45 +0100

    idk

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <CurrencyConvertor />
+</template>
+
+<script>
+import CurrencyConvertor from './components/CurrencyConvertor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CurrencyConvertor,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/CurrencyConvertor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="currency-convertor">
+    <p>
+      <small>{{ state.value_from }}</small>
+    </p>
+    <select
+      name="cuurency_from"
+      id="currency_from"
+      v-model="state.currency_from"
+    >
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="mercedes">Mercedes</option>
+      <option value="audi">Audi</option>
+    </select>
+    <span> </span>
+    <select name="cuurency_to" id="currency_to">
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="mercedes">Mercedes</option>
+      <option value="audi">Audi</option>
+    </select>
+    <br />
+    <input
+      type="number"
+      placeholder="currency_value"
+      v-model="state.value_from"
+    />
+    <span> </span>
+    <input type="number" placeholder="currency_value" v-model="to_value" />
+  </div>
+</template>
+<script>
+import { reactive } from 'vue';
+
+export default {
+  name: 'CurrencyConvertor',
+
+  setup() {
+    const state = reactive({
+      currency_from: 'CZK',
+      value_from: 12,
+      currency_to: 'USD',
+      value_to: 23,
+    }); //end of state
+
+    return {
+      state,
+    };
+  }, // end of setup
+};
+</script>
+<style scoped></style>
